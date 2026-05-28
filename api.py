fcd4434f35b3114e88bb613b238580d2a794fba8
Pheletso8 <pheletso.marumoloe8@gmail.com>
2026-05-28 22:54:21 +0200

    Fix:added my vercel app to allowed origins

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://your-frontend-domain.vercel.app"
+        "https://prompt-pal-six.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
