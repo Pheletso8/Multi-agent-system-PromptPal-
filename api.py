d9cb11e54baf02a91cb2ddf0e1de2b67e8de4ef7
Pheletso8 <pheletso.marumoloe8@gmail.com>
2026-05-28 23:06:12 +0200

    Make CORS bulletproof by allowing all origins

--- api.py
+++ api.py
@@ -17,11 +17,8 @@ app = FastAPI(title="Grade 7 Tutor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://prompt-pal-six.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
